53d940767011c70c7f51209cebe3915bc33cdeec
Delta <alihansaut62@gmail.com>
2026-05-12 00:05:57 +0500

    Add files via upload

---
+++ ASC++-main/ConsoleApplication1/ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	std::cout << "бляя что за хуния\n\n" << "blaah\n\n";
+	std::cout << "\"\n\tasda\n";
+	system("pause");
+	return 0;
+}
